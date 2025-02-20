87a041f8d718b28c2a290f8d4356caf4d87863b9
Danish <danishmaniyar2110@gmail.com>
2025-02-20 14:40:59 +0530

    Added some beans in spring.xml and created reference for computer in Dev classes

--- SpringDemoAutowiring/src/main/java/org/example/Dev.java
+++ SpringDemoAutowiring/src/main/java/org/example/Dev.java
@@ -1,9 +1,17 @@
 package org.example;
 
 public class Dev {
+
+    private Computer com;
+
+    public void setCom(Computer com){
+        this.com=com;
+    }
+
     public void build(){
 
         System.out.println("working on a project");
 
+        com.compile();
     }
 }
