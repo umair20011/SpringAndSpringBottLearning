c68548fa30ff2a05f2dafb010a4d53529a0312a4
Danish <danishmaniyar2110@gmail.com>
2025-02-20 22:47:41 +0530

    added one more controller for login means more than one controller class

---
+++ SimpleWebApp/src/main/java/com/example/SimpleWebApp/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.SimpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "You are in login";
+    }
+    
+}
