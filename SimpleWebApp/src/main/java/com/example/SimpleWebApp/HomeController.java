82f5c44add0957ea773ad826ca5359676673e49c
Danish <danishmaniyar2110@gmail.com>
2025-02-20 22:36:30 +0530

    added @RestController and @RequestMapping() annotations

--- SimpleWebApp/src/main/java/com/example/SimpleWebApp/HomeController.java
+++ SimpleWebApp/src/main/java/com/example/SimpleWebApp/HomeController.java
@@ -1,10 +1,15 @@
 package com.example.SimpleWebApp;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
+    @RequestMapping("/")
     public String greet(){
         return "Welcome to project";
     }
