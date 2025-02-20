771fbacad591fe8e8773683146dc602a02307a84
Danish <danishmaniyar2110@gmail.com>
2025-02-20 14:24:09 +0530

    added spring.xml file and added bean configuration in spring.xml

--- SpringDemoAutowiring/src/main/java/org/example/App.java
+++ SpringDemoAutowiring/src/main/java/org/example/App.java
@@ -1,14 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args ) {
 
-        Dev obj=new Dev();
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+
+        Dev obj=(Dev)context.getBean("dev");
         obj.build();
 
 
