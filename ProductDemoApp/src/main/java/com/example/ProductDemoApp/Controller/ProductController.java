406ce78fe67fe786bd0b889eec2c454181e27ef0
Danish <danishmaniyar2110@gmail.com>
2025-02-21 23:45:53 +0530

    Added code in ProductController class to return data to client

--- ProductDemoApp/src/main/java/com/example/ProductDemoApp/Controller/ProductController.java
+++ ProductDemoApp/src/main/java/com/example/ProductDemoApp/Controller/ProductController.java
@@ -1,13 +1,24 @@
 package com.example.ProductDemoApp.Controller;
 
+import com.example.ProductDemoApp.Model.Product;
+import com.example.ProductDemoApp.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
     @RequestMapping("/product")
-    public String getProducts(){
-        return "Hello";
+    public List<Product> getProducts(){
+
+        return productService.getProduct();
+
     }
 
 }
--- ProductDemoApp/src/main/java/com/example/ProductDemoApp/Service/ProductService.java
+++ ProductDemoApp/src/main/java/com/example/ProductDemoApp/Service/ProductService.java
@@ -1,13 +1,19 @@
 package com.example.ProductDemoApp.Service;
 
 import com.example.ProductDemoApp.Model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
+//@Data
+//@AllArgsConstructor
+@Component
 public class ProductService {
 
-    List<Product> products= Arrays.asList(
+    private List<Product> products= Arrays.asList(
             new Product(101,"iphone",200),
             new Product(102,"laptop",300),
             new Product(103,"fan",200)
