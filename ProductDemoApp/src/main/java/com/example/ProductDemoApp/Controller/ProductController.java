196e6f52e955dd666c16dda049a1bfa490b0b5ef
Danish <danishmaniyar2110@gmail.com>
2025-02-21 22:58:48 +0530

    added code in ProductService class to get products

--- ProductDemoApp/src/main/java/com/example/ProductDemoApp/Controller/ProductController.java
+++ ProductDemoApp/src/main/java/com/example/ProductDemoApp/Controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.example.ProductDemoApp.Controller;
 
-public class ProductController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ProductController {
+    @RequestMapping("/product")
     public String getProducts(){
-        return "";
+        return "Hello";
     }
 
 }
--- ProductDemoApp/src/main/java/com/example/ProductDemoApp/Service/ProductService.java
+++ ProductDemoApp/src/main/java/com/example/ProductDemoApp/Service/ProductService.java
@@ -1,9 +1,20 @@
 package com.example.ProductDemoApp.Service;
 
+import com.example.ProductDemoApp.Model.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ProductService {
 
-    public String getProduct(){
-        return "";
+    List<Product> products= Arrays.asList(
+            new Product(101,"iphone",200),
+            new Product(102,"laptop",300),
+            new Product(103,"fan",200)
+    );
+
+    public List<Product> getProduct(){
+        return products;
     }
 
 }
