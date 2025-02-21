fa9a55442c3be3cf04088657eecda76fc5fd0fa7
Danish <danishmaniyar2110@gmail.com>
2025-02-21 22:10:54 +0530

    Created a ProductDemoApp project

---
+++ ProductDemoApp/src/main/java/com/example/ProductDemoApp/ProductDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDemoAppApplication.class, args);
+	}
+
+}
