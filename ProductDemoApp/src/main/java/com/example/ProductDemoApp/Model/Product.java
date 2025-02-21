702bb062a36c80f47073134e47ac773fc1d222a0
Danish <danishmaniyar2110@gmail.com>
2025-02-21 22:26:57 +0530

    used @Data and @AllArgsConstructor annotation of Lombok for creating getters and setter and constructors

--- ProductDemoApp/src/main/java/com/example/ProductDemoApp/Model/Product.java
+++ ProductDemoApp/src/main/java/com/example/ProductDemoApp/Model/Product.java
@@ -1,7 +1,11 @@
 package com.example.ProductDemoApp.Model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Product {
 
     private int prodId;
