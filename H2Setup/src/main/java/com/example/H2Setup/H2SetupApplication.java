ffe734f1006362ce7b952d897b177e84542b65d3
Danish <danishmaniyar2110@gmail.com>
2025-03-02 15:41:40 +0530

    created new project named H2Setup

---
+++ H2Setup/src/main/java/com/example/H2Setup/H2SetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.H2Setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2SetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2SetupApplication.class, args);
+	}
+
+}
